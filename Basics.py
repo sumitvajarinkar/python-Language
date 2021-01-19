a581e33d30e3b0dadbeda7530d0b531a87c09366
Sumit Vajarinkar <vajarinkarss@gmail.com>
2021-01-19 20:34:36 +0530

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,23 @@
+print('Hello World!')
+
+#variable used for simplification, reduce repeated work
+# we can store it in variable and manupilate it anywhere we want in program
+#variable name start only with _(underscore), letter but not with number [x-1=4(wrong)]
+#spaces are not allow expect _(underscore)
+#avoid keywords as a variable[if,else,etc]
+
+# Data types
+# numbers, strings,lists,tuple,dictionary
+
+#string
+msg="I am sumit" 
+print(msg)
+#number
+x=1     
+print(x)
+
+# How to find data type ?
+print(type(msg)) 
+# o/p <class 'str'>
+print(type(x)) 
+# o/p <class 'int'>
