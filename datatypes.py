9bc870356b83f92c39790a109b183a9f6a600edf
Sumit Vajarinkar <vajarinkarss@gmail.com>
2021-01-21 20:41:14 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,59 @@
+# integers
+
+add=2 + 3
+print(add)
+sub=5 + 3
+print(sub)
+mul=2 * 3
+print(mul)
+div=21 / 3
+print(div)
+# power(exponential)
+power=2 ** 3
+print(power)
+
+# floats
+x=1.2
+print(type(x))
+print(x)
+
+# round()
+add=2.34 + 3.21
+print(add)
+print(round(add,2))
+sub=5.1 + 3.0
+print(sub)
+print(round(sub,2))
+mul=2.13 * 3.1
+print(mul)
+print(round(mul,2))
+div=21.5 / 3.0
+print(div)
+print(round(div,2))
+print(round(div))
+# power(exponential)
+power=2.5 ** 3.5
+print(power)
+print(round(power,2))
+
+# when you divide int/int you will get answer in float
+
+# data type conversion
+x=4/2
+print(x)
+print(int(x))
+
+f=4
+print(float(f))
+
+# multiple assignments
+a,b,c=1,2,3
+print(a,b,c)
+
+# constant life throughout the lifeof program
+# python dont have constant
+
+# If you want to use constant keep it UPPERCASE
+MAX_SIZE= 12
+MAX_SIZE=100
+print(MAX_SIZE)
