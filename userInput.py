203aae2ff8243a049b11068c519ce7ff30c8b777
Sumit Vajarinkar <vajarinkarss@gmail.com>
2021-02-10 09:02:32 +0530

    Update userInput.py

--- userInput.py
+++ userInput.py
@@ -1,25 +1,25 @@
-# #input by default takes Strings 
+#input by default takes Strings 
 
-# # string input
-# name=input('Enter your name: ')#sumit
-# print(name) #sumit
-# print(type(name)) #<class
+# string input
+name=input('Enter your name: ')#sumit
+print(name) #sumit
+print(type(name)) #<class
 
-# # int input
-# name=int(input('Enter any number: '))#56
-# print(name) #56
-# print(type(name)) #<class 'int'>
+# int input
+name=int(input('Enter any number: '))#56
+print(name) #56
+print(type(name)) #<class 'int'>
 
-# # float input
-# name=float(input('Enter any number: '))#8.9 
-# print(name) #8.9
-# print(type(name)) #<class 'float'>
+# float input
+name=float(input('Enter any number: '))#8.9 
+print(name) #8.9
+print(type(name)) #<class 'float'>
 
-# number=input('Enter a nummber: ') #4
-# number=float(number)
-# number+=1
-# print(number) #5.0
-# print(type(number)) #<class 'float'>
+number=input('Enter a nummber: ') #4
+number=float(number)
+number+=1
+print(number) #5.0
+print(type(number)) #<class 'float'>
 
 # List
 numbers=(input('Enter the Numbers:')).split() #1 2 3 4
@@ -28,4 +28,3 @@ print(type(numbers)) #<class 'list'>
 
 # what is split ?
 # it separate each element with space
-
